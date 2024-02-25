38203b5db1f4027ca7f0b81c7a5b47b0008504a7
Amin Sepehri <aminrpg@gmail.com>
2024-02-25 19:33:45 +0330

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,10 +1,15 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\Pages;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/', 'Contact::index');
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);
+
 
 service('auth')->routes($routes);
