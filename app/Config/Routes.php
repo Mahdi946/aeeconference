4ad972b9919a5fbbf69a9b9c93c63cb2900283fc
Amin Sepehri <aminrpg@gmail.com>
2024-02-27 16:42:05 +0330

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,10 +6,17 @@ use App\Controllers\Pages;
 /**
  * @var RouteCollection $routes
  */
+
+$routes->group('{locale}', function ($routes) {
+    $routes->get('/', 'Home::index');
+    $routes->get('contact', 'Contact::index');
+    // $routes->get('(:segment)', [Pages::class, 'view']);
+});
+
 $routes->get('/', 'Home::index');
-$routes->get('/', 'Contact::index');
-$routes->get('pages', [Pages::class, 'index']);
-$routes->get('(:segment)', [Pages::class, 'view']);
+$routes->get('contact', 'Contact::index');
+// $routes->get('(:segment)', [Pages::class, 'view']);
 
 
 service('auth')->routes($routes);
+
