c0b2984a04c6df8a5419925fe0056561b0486d7f
Mohammad_Shiasy <shiasy@hotmail.com>
2024-02-28 14:51:52 +0330

    prize-overview

---
+++ app/Views/prize-overview-en.php
@@ -0,0 +1,139 @@
+<?= $this->extend('templates/en') ?>
+<?= $this->section('content') ?>
+
+    <!-- Start Page Title Area -->
+    <div class="page-title-area item-bg-5">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="container">
+                    <div class="page-title-content">
+                        <h2>UNESCO Prize at a Glance</h2>
+                        <ul>
+                            <li><a href="<?=base_url('/')?>">Home</a></li>
+                            <li>UNESCO Prize at a Glance</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Page Title Area -->
+
+    <!-- Start Project Details Area -->
+    <section class="project-details-area ptb-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    <div class="project-details-image">
+                        <img src="<?=base_url('assets/img/services-details/1.jpg')?>" alt="projects">
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-md-6">
+                    <div class="project-details-image">
+                        <img src="<?=base_url('assets/img/services-details/2.jpg')?>" alt="projects">
+                    </div>
+                </div>
+
+                <div class="col-lg-12 col-md-12">
+                    <div class="projects-details-desc">
+                        <h3>UNESCO Prize at a Glance</h3>
+
+
+                        <div class="features-text">
+                            <h4>About the Prize</h4>
+                            <p>The advancement of technology and innovation is vital for the growth and success of every society, encompassing scientific, economic, social, cultural, and educational aspects. An effective approach involves observing and developing talent and guiding individuals toward commercializing their ideas. To this end, the UNESCO Technology and Innovation Prize in the Islamic Republic of Iran, in partnership with the Ministry of Science, Research and Technology, the National Commission for UNESCO-Iran, and the University of Tehran, will continue to be awarded based on established guidelines.</p>
+                            <p>This prize encourages researchers and experts to embrace a new outlook on technology and the economy. It inspires them to present innovative studies, conduct research, and offer products and services that align with technological advancements. </p>
+                        </div>
+                        <div class="features-text">
+                            <h4>Objectives of the UNESCO Technology and Innovation Prize</h4>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                Highlighting the impact of technology and innovation on improving the quality of life and promoting global peace.
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                Encouraging innovators and technologists to produce technology for the well-being of humanity.
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                Identifying technological and innovative capacities in production and services.
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                Promoting and developing creativity, innovation, and technology-based entrepreneurship and exchanging scientific and technological ideas.
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                Emphasizing the vital role of technology and innovation in solving major issues and encouraging the use of innovative technologies to address existing challenges.
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                Creating the necessary platforms for curiosity to explore the world from new perspectives and developing a creative society.
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                Facilitating international interactions among companies, universities, and policymakers to elevate the status of scientists and technologists through recognition.
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                Discovering investment opportunities in the research and development sector to promote innovative technologies.
+                            </p>
+                        </div>
+
+                        <div class="features-text">
+                            <h4>Who is eligible for the prize?</h4>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                Individuals and technology teams engaged in professional activities.
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                Owners of startups, SMEs, and large innovative, technological, and knowledge-based companies.
+                            </p>
+                            <p>
+                                This prize will cover the entire technology and innovation ecosystem, from education to science and technology parks and growth centers. It is noteworthy that all eligible individuals will be evaluated equally regardless of gender, religion, race, ethnicity, nationality, class, belief, language, sexual orientation, gender identity, age, health status, and other characteristics.
+                            </p>
+                        </div>
+
+
+
+                        <div class="project-details-info">
+                            <div class="single-info-box">
+                                <h4>author</h4>
+                                <span>James Anderson</span>
+                            </div>
+
+                            <div class="single-info-box">
+                                <h4>Category</h4>
+                                <span>Nomination</span>
+                            </div>
+
+                            <div class="single-info-box">
+                                <h4>Date</h4>
+                                <span>February 28, 2024</span>
+                            </div>
+
+                            <div class="single-info-box">
+                                <h4>Share</h4>
+                                <ul class="social">
+                                    <li><a href="#" target="_blank"><i class="flaticon-facebook"></i></a></li>
+                                    <li><a href="#" target="_blank"><i class="flaticon-twitter"></i></a></li>
+                                    <li><a href="#" target="_blank"><i class="flaticon-pinterest"></i></a></li>
+                                    <li><a href="#" target="_blank"><i class="flaticon-instagram"></i></a></li>
+                                </ul>
+                            </div>
+
+                            <div class="single-info-box d-none">
+                                <a href="#" class="default-btn">Live Preview</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Project Details Area -->
+
+
+<?= $this->endSection() ?>
---
+++ app/Views/prize-overview-fa.php
@@ -0,0 +1,141 @@
+
+<?= $this->extend('templates/fa') ?>
+<?= $this->section('content') ?>
+
+    <!-- Start Page Title Area -->
+    <div class="page-title-area item-bg-5">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="container">
+                    <div class="page-title-content">
+                        <h2>جایزه یونسکو در یک نگاه</h2>
+                        <ul>
+                            <li><a href="<?= base_url('/'); ?>">خانه</a></li>
+                            <li>جایزه یونسکو در یک نگاه</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Page Title Area -->
+
+    <!-- Start Project Details Area -->
+    <section class="project-details-area ptb-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    <div class="project-details-image">
+                        <img src="<?=base_url('assets-rtl/img/services-details/1.jpg')?>" alt="projects">
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-md-6">
+                    <div class="project-details-image">
+                        <img src="<?=base_url('assets-rtl/img/services-details/2.jpg')?>" alt="projects">
+                    </div>
+                </div>
+
+                <div class="col-lg-12 col-md-12">
+                    <div class="projects-details-desc">
+                        <h3>جایزه یونسکو در یک نگاه</h3>
+
+
+                        <div class="features-text">
+                            <h4>درباره جایزه</h4>
+                            <p>توسعه فناوری و نوآوری، توجه به مولفه‌های علمی، اقتصادی، اجتماعی، فرهنگی و آموزشی آن به عنوان رکن شکوفایی هر جامعه ای از کاربست منسجمی برخوردار است. این کاربست مبتنی بر رصد و توان افزایی، هدایت و پرورش استعدادها و تجاری‌سازی در حوزه‌های فناوری و نوآوری است. جایزه فناوری و نوآوری یونسکو در جمهوری اسلامی ایران، به منظور تشویق و ترغیب ذینفعان این بخش و شناسایی ظرفیت‌های فناوری و نوآوری با همکاری وزارت علوم، تحقیقات و فناوری، کمیسیون ملی یونسکو-ایران و دانشگاه تهران مطابق با ضوابط و مقررات به مدت نامحدود اعطا خواهد شد.</p>
+                        </div>
+                        <div class="features-text">
+                            <h4>اهداف جایزه فناوری و نوآوری یونسکو</h4>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                برجسته نمودن تأثیر فناوری و نوآوری بر ارتقای کیفیت زندگی و گسترش صلح در جامعه جهانی؛
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                تشويق و ترغيب نوآوران و فناوران جهت تولید فناوری در راستای سعادت بشری؛
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                شناسایی ظرفیت‌های فناوری و نوآوری در حوزه‌های تولیدی و خدماتی؛
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                ترویج و توسعه خلاقیت، نوآوری و کارآفرینی مبتنی بر فناوری و تبادل ایده‌های علمی و فناورانه؛
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                بیان نقش حیاتی فناوری و نوآوری در حل چالش‌های بزرگ جهان و تشویق به استفاده از فناوری‌های نوآورانه جهت حل چالش‌های موجود؛
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                ايجاد بسترهای لازم جهت کنجکاوی برای کشف جهان از دیدگاه‌های جدید و توسعه جامعه‌ای خلاق؛
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                تسهیل ایجاد زمینه تعاملات بین‌المللی بین شرکت‌ها، دانشگاه‌ها و سیاستگذاران با هدف بالابردن موقعیت دانشمندان و فناوران با تجليل از آن‌ها
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                كشف فرصت‌های سرمايه گذاری در بخش تحقیق و توسعه به منظور توسعه فناوری‌های نوآورانه.
+                            </p>
+                        </div>
+
+                        <div class="features-text">
+                            <h4>متقاضیان جایزه</h4>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                افراد و تیم‌های فناوری که دارای فعالیت‌های حرفه‌ای می‌باشند؛
+                            </p>
+                            <p>
+                                <i class="flaticon-tick"></i>
+                                صاحبان شرکت‌های نوپا، SM Eها و شرکت‌های بزرگ نوآور، فناور و دانش بنيان؛
+                            </p>
+                            <p>
+                                این جایزه اکوسیستم فناوری و نوآوری جهان را از دانشگاه‌ها تا پارک‌های علم و فناوری و مراکز رشد را پوشش خواهد داد. لازم به ذکر است کلیه واجدین به طور برابر و صرف نظر از جنسیت، دین، نژاد، قومیت، ملیت، طبقه، اعتقاد، زبان، گرایش جنسی، هویت جنسیتی، سن، وضعیت سلامتی و خصوصیات دیگر مورد ارزیابی قرار خواهند گرفت.
+                            </p>
+                        </div>
+
+
+
+
+
+                        <div class="project-details-info">
+                            <div class="single-info-box">
+                                <h4>نویسنده</h4>
+                                <span>امین سپهری</span>
+                            </div>
+
+                            <div class="single-info-box">
+                                <h4>دسته</h4>
+                                <span>برگزیدگان</span>
+                            </div>
+
+                            <div class="single-info-box">
+                                <h4>تاریخ</h4>
+                                <span>28 فردردین, 1403</span>
+                            </div>
+
+                            <div class="single-info-box">
+                                <h4>Share</h4>
+                                <ul class="social">
+                                    <li><a href="#" target="_blank"><i class="flaticon-facebook"></i></a></li>
+                                    <li><a href="#" target="_blank"><i class="flaticon-twitter"></i></a></li>
+                                    <li><a href="#" target="_blank"><i class="flaticon-pinterest"></i></a></li>
+                                    <li><a href="#" target="_blank"><i class="flaticon-instagram"></i></a></li>
+                                </ul>
+                            </div>
+
+                            <div class="single-info-box d-none">
+                                <a href="#" class="default-btn">Live Preview</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Project Details Area -->
+
+
+<?= $this->endSection() ?>
