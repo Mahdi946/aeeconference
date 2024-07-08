c35333cfa6f4c6f3c0ab1a23d3428064c138417d
taha <tahamzahry@gmail.com>
2024-07-08 16:29:18 +0430

    Add users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,13 +14,34 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('Name');
+            $table->string('Family');
+            $table->string('Name_fa');
+            $table->string('Family_fa');
+            $table->string('FatherName')->nullable();
+            $table->bigInteger('BirthCert',NULL,true)->unique()->nullable();
+            $table->bigInteger('NationalCode',NULL,true)->unique()->nullable();
+            $table->date('BirthDay')->nullable();
+            $table->bigInteger('MobileNumber');
+            $table->bigInteger('PhoneNumber');
+            $table->string('Email')->unique();
+            $table->string('Field');
+            $table->string('Edu');
+            $table->string('Job')->nullable();
+            $table->text('Address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('Password');
+            $table->string('City');
+            $table->string('City_fa');
+            $table->string('Country');
+            $table->string('Rank');
+            $table->text('Org');
+            $table->text('Org_fa');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::statement('ALTER TABLE users MODIFY BirthCert  BIGINT(10) UNSIGNED ZEROFILL  NULL');
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
