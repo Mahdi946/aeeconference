72ded8ac12346100a2d7e867989abcc7f5ab29cf
Alix24 <alimohammadi123450@gmail.com>
2024-07-08 11:50:25 +0330

    fa template

---
+++ resources/views/awards-fa.blade.php
@@ -0,0 +1,103 @@
+@extends('templates.fa')
+@section('content')
+
+<!-- Start Page Title Area -->
+<div class="page-title-area item-bg-5">
+    <div class="d-table">
+        <div class="d-table-cell">
+            <div class="container">
+                <div class="page-title-content">
+                    <h2>جوایز</h2>
+                    <ul>
+                        <li><a href="{{ url('/') }}>">خانه</a></li>
+                        <li>جوایز</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Page Title Area -->
+
+<!-- Start Project Details Area -->
+<section class="project-details-area ptb-100">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-3 col-md-3 ">
+                <div class="single-counter">
+                    <div class="icon">
+                        <i class="flaticon-trophy"></i>
+                    </div>
+
+                    <h3>
+                        <span style="direction: ltr;" class=" counters" data-countValue="3000">0</span>
+                    </h3>
+                    <p> دلار </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row ">
+
+
+            <div class="col-lg-12 col-md-12">
+                <div class="projects-details-desc">
+                    <h3>جوایز</h3>
+
+                    <p>جایزه در یكی از دوحالت زیر به برگزیدگان اعطا میگردد كه شامل مزایای مادی و معنوی از جمله تقدیرنامه، نشان جایزه فناوری و نوآوری و هدیه ویژه است. </p>
+
+                    <div class="features-text">
+                        <h4><i class="flaticon-tick"></i> برگزیدگان برتر: </h4>
+                        <p>آن دسته از برگزیدگان خواهند بود كه دارای بالاترین امتیاز در حوزه فناوری و نوآوری ذیربط بوده و حد نصاب امتیاز كمیته علمی و ارزیابی را نیز كسب نموده باشند.</p>
+                    </div>
+
+                    <div class="features-text">
+                        <h4><i class="flaticon-tick"></i> برگزیدگان شایسته تقدیر: </h4>
+                        <p>آن دسته از برگزیدگان خواهند بود كه حد نصاب امتیاز لازم جهت برگزیدگان برتر را كسب ننموده اند، ولی با پیشنهاد كمیته علمی و ارزیابی و تائید شورای سیاستگذاری، جایزه به آنان اعطا می شود.</p>
+                    </div>
+
+
+                    <div class="features-text">
+                        <h4> ** </h4>
+                        <p>« میزان و نوع هدیه ویژه در هر دوره توسط شورای سیاست گذاری تعیین و اعلام می شود. »</p>
+                    </div>
+
+                    <div class="project-details-info d-none">
+                        <div class="single-info-box">
+                            <h4>نویسنده</h4>
+                            <span>امین سپهری</span>
+                        </div>
+
+                        <div class="single-info-box">
+                            <h4>دسته</h4>
+                            <span>جایزه</span>
+                        </div>
+
+                        <div class="single-info-box">
+                            <h4>تاریخ</h4>
+                            <span>28 فردردین, 1403</span>
+                        </div>
+
+                        <div class="single-info-box">
+                            <h4>Share</h4>
+                            <ul class="social">
+                                <li><a href="#" target="_blank"><i class="flaticon-facebook"></i></a></li>
+                                <li><a href="#" target="_blank"><i class="flaticon-twitter"></i></a></li>
+                                <li><a href="#" target="_blank"><i class="flaticon-pinterest"></i></a></li>
+                                <li><a href="#" target="_blank"><i class="flaticon-instagram"></i></a></li>
+                            </ul>
+                        </div>
+
+                        <div class="single-info-box d-none">
+                            <a href="#" class="default-btn">Live Preview</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Project Details Area -->
+
+
+@endSection
---
+++ resources/views/templates/fa.blade.php
@@ -0,0 +1,419 @@
+<!doctype html>
+<html lang="fa" dir="rtl">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="Novin Tarh Pars">
+    <!-- Send your Resume to Jobs [ at ] NovinTarhPars.com -->
+    <!--
+         Novin Tarh Pars
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓▓▓▓▓▓▓▓▓▓▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓█████████████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░▒▓███▓░░░░░░░░▓███▒░░░░░▓███░░░░░░░░░███▓▒░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░▒▓████████░░▒▓▓██████░░░░░░▓█████▓▓▒▒░▒███████▓▒░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░▒██████▓▒▒█████████████▓░░░░░░░▓█████████████▒▓██████▓░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░▒████▓░░░░░░████▓▒▒░░░░░░░░░░░░░░░░░░░▒▓▓███▓░░░░░▒█████░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░▒████░░░░░░░░░░░░░░░░▒░░░░░░░░░░░░░░░░░░░░░░░░░░░▒████░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░▓████░░░░░░░░░░░░▒▓███░░░░░░░░░░░░░░░░░░░░░░░░░░████▒░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░▓████▓░░░░░░░░░░▒███████▓░░░░░░░░░░░░░░░░░░░░░░░░░▒█████▒░░░░░░░░░░░░░░░░
+░░░░░░░░▒███▓▒░▓████▒░░░░░░░░░░▓████▓▒░▓██▒░░░░░░░░░░░░░░░░░░░░░░░░░░▒█████▒░▒▓██▓░░░░░░░░
+░░░░░░░▓██████████▒░░░░░░░░░░▒████▒░░░░░███░░░░▒▒▓█████████████▓▓▒░░░░░▒██████████▓░░░░░░░
+░░░░░░▓███▓░▓███▓░░░░░░░░░░░░████░░░░░░░███░░▓█████▓▓▒▒▒▒▒▒▓▓▓██████▓░░░░▒███▓▒▒████░░░░░░
+░░░░░████▒░░░░░░░░░░░░░░░░░░▓███░░░░░░░░███░░░▒▒░░░░░░░░░░░░░░░░░▒███▒░░░░░░░░░░░████░░░░░
+░░░░████▒░░░░░░░░░██████████████▓░░░░░░▓██▓░░░░░░░░▒▓▓▓▓▓▓▓▓▓▓▒░░░▒██▒░░░░░░░░░░░░████▒░░░
+░░░░██████▓░░░░░░░▓████▓▓▓▓▓▓██████▒░░▒███░░░░░░░▓▓▒░░░░░░░░░░▓▓░░▒██▒░░░░░░░░░░▓█████▓░░░
+░░░░░░▒████▓░░░░░░░███▒░░░░░░░░▒████▒░▓▓▒░░░░░░░░░░░░░░░░░░░░░▒▓░░▒██▒░░░░░░░░░████▓▒░░░░░
+░░░░░░░████░░░░░░░░▓██▓░░░░░░░░░░▓███▒░░▒▒▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░▒▓░░▒██▒░░░░░░░░░▓███▒░░░░░░
+░░░░░░▓███▒░░░░░░░░░███▓░░░░░░░░░░███▓░░▓███████████▒░░░░░░░░░▒▓░░▒██▒░░░░░░░░░░████░░░░░░
+░░░░░░████░░░░░░░░░░░▓███▓░░░░░░░░████░░▓███████████▒░░░░░░░░░▒▓░░▒██▒░░░░░░░░░░▒███▒░░░░░
+▒▓▓▓▓████▒░░░░░░░░░░░░░▓█████▓▓▓▒░████░░▓███████████▒░░░░░░░░░▒▓░░▒██▒░░░░░░░░░░░████▓▓▓▒░
+█████████░░░░░░░░░░░░░░░░▒▓██████▒████░░▓███████████▒░░░░░░░░░▒▓░░▒██▒░░░░░░░░░░░▓████████
+████░░░░░░░░░░░░░░░░░░░░░░▓▓▓▓▓▒░░████░░▓█████▓▓▓▓▓▓░░░░░░░░░░▒▓░░▒██▒░░░░░░░░░░░░▒▒▒▒▓███
+████░░░░░░░░░░░░░░░░░░░░░░█████▒░░████░░▓█████▒░░░░░░░░░░░░░░░▒▓░░▒██▒░░░░░░░░░░░░░░░░▓███
+████░░░░░░░░░░░░░░░░░░░░░░█████▒░░████░░▓█████▒░░░░░░░░░░░░░░░▒▓░░▒██▒░░░░░░░░░░░░░░░░▓███
+████████▒░░░░░░░░░░░░░░░░░█████▒░░████░░▓█████▒░░░░░░░░░░░░░░░▒▓░░▒██▒░░░░░░░░░░░░▓▓▓▓████
+▓████████░░░░░░░░░░░░░░░░░█████▒░░████░░▓█████▒░░░░░░░░░░░░░░░▒▓░░▒██▒░░░░░░░░░░░▓████████
+░░░░░▒███▓░░░░░░░░░░░░░░░░█████▒░░████░░▓█████▒░░░░░░░░░░░░░░░▒▓░░▒██▒░░░░░░░░░░░███▓░░░░░
+░░░░░░████░░░░░░░░░░░██████████▒░░████░░▓█████▒░░░░░░░░░░░░░░░▒▓░░▒██▒░░░░░░░░░░▓███░░░░░░
+░░░░░░▒███▓░░░░░░░░░░██████████▒░░████░░▓█████▒░░░░░░░░░░░░░░░▒▓░░▒██▒░░░░░░░░░░███▓░░░░░░
+░░░░░░░████▒░░░░░░░░░██████████▒░░████░░▓█████▒░░░░░░░░░░░░░░░▒▓░░▒██▒░░░░░░░░░████░░░░░░░
+░░░░▓██████▒░░░░░░░░░██████████▒░░████░░▓█████▒░░░░░░░░░░░░░░░▒▓░░▒██▒░░░░░░░░░█████▓▒░░░░
+░░░▓████▓░░░░░░░░░░░░██████████▒░░████░░▓█████▒░░░░░░░░░░░░░░░▒▒░░▒██▒░░░░░░░░░░▒▓████▓░░░
+░░░░▓███▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒██▒░░░░░░░░░░░▒████░░░░
+░░░░░▒████░░░▒▓▓░░░░░░░▒▒▓▓█████████████▓▓▒░░▒▓▓███████████████▓▓▒▓██▒░░░░░▒▒░░░▓███▓░░░░░
+░░░░░░▒██████████▒░░░████████▓▓▓▓▒▒▓▓▓▓█████████▓▓▓▒▒▒▒▒▒▒▒▓▓▓███████▒░░░▓████▓████▒░░░░░░
+░░░░░░░░█████▓▓████▒░░▓▓▒░░░░░░░░░░░░░░░░░░▒▒▒░░░░░░░░░░░░░░░░░░░░▒▓▓░░▓██████████▒░░░░░░░
+░░░░░░░░░▒▒▒░░░▒█████▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓████▓░░░▒▓▓░░░░░░░░░
+░░░░░░░░░░░░░░░░░▒█████▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█████▓░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒████░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░▒████░░░░░░░▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒░░░░░░▒████░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░▒█████▓▒░░░███████▓▒▒▒░░░░░░░░░░░░░▒▒▓▓██████░░░▒▓█████▒░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░▒▓██████████▓█████████▓░░░░░░███████████████▓█████▓▒░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░▓██████░░░░░▒▒▓████░░░░░░████▓▒▒▒░░░▒██████▓▒░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░▒▓▒░░░░░░░░░████▓▓▓▓▓▓███▓░░░░░░░░░▓▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓████████████▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+                                     -->
+    <link rel="stylesheet" href="{{ asset('assets-rtl/css/bootstrap.rtl.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets-rtl/css/owl.default.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets-rtl/css/owl.carousel.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets-rtl/css/magnific-popup.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets-rtl/css/animate.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets-rtl/css/boxicons.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets-rtl/css/flaticon.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets-rtl/css/meanmenu.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets-rtl/css/odometer.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets-rtl/css/style.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets-rtl/css/responsive.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets-rtl/css/responsive.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('assets-rtl/css/rtl.css') }}">
+
+    <title> کنگره ملی ترویج و آموزش کشاورزی،منابع طبیعی و محیط زیست پایدار </title>
+
+    <link rel="icon" type="image/png" href="{{ asset('assets-rtl/img/favicon.png') }}">
+    <link rel="stylesheet" href="{{ asset('assets/leaflet/leaflet.css') }}" />
+
+</head>
+
+<body>
+
+    <!-- Start Preloader Area -->
+    <div class="preloader">
+        <div class="preloader">
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <!-- End Preloader Area -->
+
+    <!-- Start Navbar Area -->
+    <div class="navbar-area navbar-two">
+        <div class="fria-responsive-nav">
+            <div class="container">
+                <div class="fria-responsive-menu">
+                    <div class="logo">
+                        <a href="{{ url('/') }}">
+                            <img src="{{ asset('assets-rtl/img/logo.png') }}" alt="logo" class="logo-header">
+
+                        </a>
+                        <!-- <a href="{{ url('lang/' . (\Config\Services::language()->getLocale() == 'fa' ? 'en' : 'fa')) }}" class="float-end mt-3">
+                            <small> English </small>
+                        </a> -->
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="fria-nav">
+            <div class="container">
+                <nav class="navbar navbar-expand-md navbar-light">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <img src="{{ asset('assets-rtl/img/logo.png') }}" alt="logo" style="max-width: 100px;">
+                    </a>
+
+
+                    <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="{{ url('/') }}" class="nav-link active">
+                                    صفحه اصلی
+                                </a>
+                            </li>
+
+
+
+
+
+                            <!-- <li class="nav-item">
+                                    <a href="{{ base('events') }}" class="nav-link">
+                                        رویدادها
+                                    </a>
+                                </li> -->
+
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    جوامع
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    کنفرانس
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    آموزش
+                                </a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    تماس با ما
+                                </a>
+                            </li>
+
+
+                        </ul>
+
+                        <div class="others-options">
+                            <!-- <a href="{{ url('lang/' . (\Config\Services::language()->getLocale() == 'fa' ? 'en' : 'fa')) }}" class="">
+                                <small> English </small>
+                            </a> -->
+                            @auth
+
+                                <a href="{{ url('/admin/news') }}" class="btn btn-info b-r">اخبار</a>
+                            @endauth
+                            @guest
+
+
+                                <a href="{{ url('login') }}" class="btn btn-info b-r">به ما بپیوندید</a>
+                            @endguest
+                            {{-- <!--                                <a href="--> //=('register') --}}
+                            <!--" class="default-btn">ثبت‌نام</a>-->
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+    <!-- End Navbar Area -->
+    @section('content')
+        <!-- Start Footer Area -->
+        <section class="footer-section pt-100 pb-70">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12 col-sm-12">
+                        <div class="single-footer-widget">
+                            <div class="footer-heading">
+                                <h3> ما را دنبال کنید </h3>
+                                <!-- <h3>درباره ما</h3> -->
+                            </div>
+                            <!-- <p class="text-vip-2-5">
+                                                    توسعه فناوری و نوآوری، توجه به مولفه‌های علمی، اقتصادی، اجتماعی، فرهنگی و آموزشی آن به عنوان رکن شکوفایی هر جامعه ای از کاربست منسجمی برخوردار است.
+                                                    این کاربست مبتنی بر رصد و توان افزایی، هدایت و پرورش استعدادها و تجاری‌سازی در حوزه‌های فناوری و نوآوری است.
+                                                    جایزه فناوری و نوآوری یونسکو در جمهوری اسلامی ایران، به منظور تشویق و ترغیب ذینفعان این بخش و شناسایی ظرفیت‌های فناوری و نوآوری با همکاری وزارت علوم، تحقیقات و فناوری،
+                                                    کمیسیون ملی یونسکو ایران و دانشگاه تهران مطابق با ضوابط و مقررات به مدت نامحدود اعطا خواهد شد.
+                                                </p> -->
+                            <ul class="footer-social">
+                                <li>
+                                    <a href="#">
+                                        <i class="flaticon-facebook"></i>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="#">
+                                        <i class="flaticon-twitter"></i>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="#">
+                                        <i class="flaticon-pinterest"></i>
+                                    </a>
+                                </li>
+
+                                <li>
+                                    <a href="#">
+                                        <i class="flaticon-instagram"></i>
+                                    </a>
+                                </li>
+                                <li>
+
+
+                                    <p>
+                                    <h6> آدرس دبیرخانه کنگره </h6>
+                                    </p>
+                                    <p>
+                                        استان البرز، کرج، بلوار امامزاده حسن (ع)،دانشکدگان کشاورزی و منابع طبیعی دانشگاه
+                                        تهران، گروه ترویج و آموزش کشاورزی، طبقه سوم، دبیرخانه دهمین کنگره ملی علوم ترویج
+                                        وآموزش کشاورزی، منابع طبیعی و محیط زیست پایدار
+                                    </p>
+
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="single-footer-widget">
+                            <div class="footer-heading">
+                                <h3>اطلاعات بیشتر</h3>
+                            </div>
+
+                            <ul class="footer-quick-links">
+                                <li>
+                                    <p>
+                                        <b>زمان برگزاری:</b> سه شنبه و چهارشنبه 8 و 9 آبان 1403
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        <b>دریافت اصل مقالات</b> : 20 تیر 1403
+                                    </p>
+                                </li>
+
+                                <li>
+                                    <p>
+                                        <b>اعلام نتابج داوری :</b> 31 شهریور 1403
+                                    </p>
+                                </li>
+                                <li>
+                                    <p>
+                                        ثبت نام جهت شرکت در کنگره حداکثر تا 15 مهر 1403
+                                    </p>
+                                </li>
+
+
+
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="single-footer-widget">
+                            <div class="footer-heading">
+                                <h3>تماس با ما</h3>
+                            </div>
+
+                            <div class="footer-info-contact">
+                                <i class="flaticon-phone-call"></i>
+                                <h3>تلفن</h3>
+                                <span><a href="tel:02632238293"> 02632238293 </a></span>
+                            </div>
+
+                            <div class="footer-info-contact">
+                                <i class="flaticon-phone-call"></i>
+                                <h3> ایتا </h3>
+                                <span> 09192426803 </span>
+                            </div>
+
+                            <div class="footer-info-contact">
+                                <i class="flaticon-envelope"></i>
+                                <h3> کد پستی </h3>
+                                <span> 3158777871 </span>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="lines">
+                <div class="line"></div>
+                <div class="line"></div>
+                <div class="line"></div>
+            </div>
+        </section>
+        <!-- End Footer Area -->
+    @endsection
+    <!-- Start Copy Right Area -->
+    <div class="copyright-area">
+        <div class="container">
+            <div class="copyright-area-content">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 col-md-6">
+                        <p>کپی رایت © {{ date('Y') }} تمام حقوق محفوظ است. طراحی و توسعه توسط
+                            <a href="https://novintarhpars.com" target="_blank">
+                                نوین طرح پارس
+                            </a>
+                        </p>
+                    </div>
+
+                    <div class="col-lg-6 col-md-6">
+                        <ul>
+                            <li>
+                                <a href="{{ url('terms-condition') }}">قوانین و مقررات</a>
+                            </li>
+                            <li>
+                                <a href="{{ url('privacy-policy') }}">حریم خصوصی</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Copy Right Area -->
+
+    <!-- Start Go Top Section -->
+    <div class="go-top">
+        <i class="bx bx-chevron-up"></i>
+        <i class="bx bx-chevron-up"></i>
+    </div>
+    <!-- End Go Top Section -->
+    <!-- dark version -->
+
+    <!-- dark version -->
+    <script src="{{ asset('assets-rtl/js/jquery.min.js') }}"></script>
+
+    <script src="{{ asset('assets-rtl/js/popper.min.js') }}"></script>
+
+    <script src="{{ asset('assets-rtl/js/bootstrap.min.js') }}"></script>
+
+    <script src="{{ asset('assets-rtl/js/jquery.meanmenu.js') }}"></script>
+
+    <script src="{{ asset('assets-rtl/js/owl.carousel.js') }}"></script>
+
+    <script src="{{ asset('assets-rtl/js/jquery.magnific-popup.min.js') }}"></script>
+
+    <script src="{{ asset('assets-rtl/js/jquery.appear.min.js') }}"></script>
+
+    <script src="{{ asset('assets-rtl/js/odometer.min.js') }}"></script>
+
+    <script src="{{ asset('assets-rtl/js/jquery.ajaxchimp.min.js') }}"></script>
+
+    <script src="{{ asset('assets-rtl/js/form-validator.min.js') }}"></script>
+
+    <script src="{{ asset('assets-rtl/js/contact-form-script.js') }}"></script>
+
+    <script src="{{ asset('assets-rtl/js/wow.min.js') }}"></script>
+
+    <script src="{{ asset('assets-rtl/js/main.js') }}"></script>
+    <script src="{{ asset('assets/js/homePrizeCountDownDate.js') }}"></script>
+    <script src="{{ asset('assets/js/CountDownDate.js') }}"></script>
+    <script src="{{ asset('assets/leaflet/leaflet.js') }}"></script>
+
+    <script>
+        $(document).ready(function() {
+            if (document.querySelector("#map") === null)
+                return;
+            //1. initialize leaflet map
+            var center = [35.73338, 51.39444];
+            var propertiesmap = L.map('map').setView(center, 17);
+
+            propertiesmap.invalidateSize();
+
+            var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+                maxZoom: 17,
+                subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+            });
+
+            googleStreets.addTo(propertiesmap)
+
+            var marker = L.marker(center).addTo(propertiesmap);
+
+        });
+    </script>
+</body>
+
+
+</html>
