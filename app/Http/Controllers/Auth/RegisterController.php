659ec711857c7d4aab69a8b2e7e90b7c1ad46d42
Amin Sepehri <aminrpg@gmail.com>
2024-07-10 18:25:44 +0330

    Redirected to Articles

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/Articles';
 
     /**
      * Create a new controller instance.
