527e1f581044bff0ab25747022d91974a8c4150a
Alix24 <alimohammadi123450@gmail.com>
2024-07-10 09:57:48 +0330

    register updated

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\web\WriterController;
+use App\Http\Controllers\web\ArticleController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +12,26 @@ Auth::routes();
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::prefix('/Articles')->controller(ArticleController::class)->name('Articles.')->group(function(){
+
+    Route::get('/getArticle','getArticle');
+
+});
+
+
+Route::resource('Articles',  ArticleController::class);
+
+
+
+Route::prefix('/Writers')->controller(WriterController::class)->name('Writers.')->group(function(){
+
+    Route::get('/getWriterByID/{article}','getWriterByID');
+    Route::post('/getEmailWriter','getEmailWriter')->name('getEmailWriter');
+
+});
+
+Route::resource('Writers',  WriterController::class);
+
+
