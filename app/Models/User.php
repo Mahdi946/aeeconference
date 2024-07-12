d5f0f49ee4267a11f40abd7db2ad3d409219fa97
Amin Sepehri <aminrpg@gmail.com>
2024-07-12 12:24:33 +0330

    Hide password

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,7 +31,11 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'Password',
+        'password',
         'remember_token',
+        'email_verified_at',
+        'created_at',
+        'updated_at',
     ];
 
     /**
@@ -44,6 +48,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'Password' => 'hashed',
+            'password' => 'hashed',
         ];
     }
 }
