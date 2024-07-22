58a497f74c4956e539e4f086854cd8a5066c96ef
Amin Sepehri <aminrpg@gmail.com>
2024-07-22 17:01:09 +0330

    Added Slider and org

--- resources/views/home-fa.blade.php
+++ resources/views/home-fa.blade.php
@@ -8,24 +8,11 @@
                 <div class="clients-slider owl-carousel owl-theme">
 
                     <div class="clients-item">
-                        <img class="img-slider" src="assets/img/slider1.jpg" alt="">
-                    </div>
-                    <div class="clients-item">
-                        <img class="img-slider" src="assets/img/slider2.jpg" alt="">
+                        <img class="img-slider" src="assets/img/slider.jpg" alt="">
                     </div>
                     <div class="clients-item">
                         <img class="img-slider" src="assets/img/slider0.jpg" alt="">
                     </div>
-                    <div class="clients-item">
-                        <img class="img-slider" src="assets/img/slider3.jpg" alt="">
-                    </div>
-                    <div class="clients-item">
-                        <img class="img-slider" src="assets/img/slider4.jpg" alt="">
-                    </div>
-                    <!-- <div class="clients-item">
-                                <img class="img-slider" src="assets/img/slider2.jpg" alt="">
-                            </div> -->
-
 
                 </div>
             </div>
@@ -40,10 +27,14 @@
 
     <section class="fun-facts-area pt-100 pb-70">
 
+        <div class="section-title">
+            <h2>سازمان کنگره</h2>
+            <div class="bar"></div>
+        </div>
 
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="single-fun-fact">
                         <div class="icon">
                             <i class="flaticon-check"></i>
@@ -57,7 +48,7 @@
                         <p> دکتر علی اسدی </p>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="single-fun-fact">
                         <div class="icon">
                             <i class="flaticon-check"></i>
@@ -71,7 +62,7 @@
                         <p> دکتر امیرعلم بیگی </p>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="single-fun-fact">
                         <div class="icon">
                             <i class="flaticon-check"></i>
@@ -85,186 +76,22 @@
                         <p> دکتر امیررضا رضایی  </p>
                     </div>
                 </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="single-fun-fact">
+                        <div class="icon">
+                            <i class="flaticon-check"></i>
+                        </div>
 
+                        <h3>
+                            <span style="direction: ltr;font-size:medium" class="">
+                                مسئول دبیرخانه علمی و اجرایی کنگره
+                            </span>
+                        </h3>
+                        <p> زهرا موسوی قیداری </p>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
 
-    <!-- Start Features Area -->
-    <section class="features-section pt-100 pb-70">
-        <div class="container">
-
-            <div class="row" dir="rtl">
-                <div class="text">
-                    <h3> فراخوان ارسال مقالات دهمین کنگره ملی علوم ترویج و آموزش کشاورزی، منابع طبیعی و محیط‌زیست پایدار</h3>
-                    <p>
-                        انجمن علمی ترويج و آموزش کشاورزی ايران مفتخر است با همکاری دانشگاه تهران، «دهمین کنگره ملی ترویج و
-                        آموزش کشاورزی، منابع طبیعی و محیط‌زیست  پایدار» با تمرکز بر «مشارکت، نوآوری و توسعه دانش بنیان» و در
-                        راستای تعمیق و توسعه دانش نظری، ارتباط هرچه بیشتر بین بخشهای دانشگاه و اجرا و تلفیق اندیشه و عمل و
-                        مسئله‌محوری در ترویج و آموزش کشاورزی، در8 و 9 آبانماه سال 1403در دانشکدگان کشاورزی و منابع طبیعی
-                        دانشگاه تهران برگزار نماید. این محفل اندیشه‌ورزی علمی فرصتی مناسب برای حضور، اندیشه‌ورزی، تدوین
-                        سیاست و برنامهریزی صاحب نظران علمی و اجرایی ترویج و آموزش کشاورزی و تقویت شبکه سازی استادان،
-                        صاحبنظران، پژوهشگران، مدیران، کارشناسان و دانشجویان این حوزه بوده و راهنمای تداوم مسیر توسعه علوم
-                        ترویج و آموزش کشاورزی و منابع طبیعی و غلبه بر چالش‌های پیش‌روی این نظام در پاسخگویی به نیازهای رو به
-                        تغییر جامعه خواهد بود. بر این اساس، از تمامی صاحبان دانش علمی و کاربردی تقاضا می‌شود با ارائه
-                        دستاوردهای علمی، پژوهشی و اجرایی خود، بر کیفیت و غنای این محفل بیافزایند.
-                    </p>
-                </div>
-            </div>
-
-            <div class="row" dir="rtl">
-                <div class="text">
-                    <h3> اهداف کنگره </h3>
-
-                </div>
-
-                <ul class="list" style="list-style: none;">
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        تحلیل الزامات و سازوکارهای توسعه منابع انسانی بخش کشاورزی
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        تبیین زمینه های تلفیق اندیشه و عمل و مسئله محوری در ترویج و آموزش کشاورزی
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        تحلیل و بررسی نظام نوین ترويج کشاورزی و ارائه راهکارهای بهبود عملکرد
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        تبیین الزامات توسعه بهره وری نظام آموزش کشاورزی تحول‌گرا، اثربخش و کارآفرین
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        فراهم نمودن فرصت تقویت شبکه دانش و اطلاعات در بخش کشاورزی و بهره مندی بهینه از تجربه های عملیاتی
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        شناسایی و تقویت زمینه‌های نوین اثرگذاری نظام‌ ترویج و آموزش بر توسعه دانش بنیان بخش کشاورزی و
-                        روستایی
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        تبیین زمینه های توسعه کسب‌وکارهای نوپا و زیستبوم های کارآفرینی در بخش کشاورزی
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        بازنگري ساختاري و محتوايي نظام آموزش عالي كشاورزي با رويكرد اشتغال فارغ‌التحصيلان و توسعه آموزش
-                        كارآفرينانه و ارتباط با صنعت و توسعه كشاورزي دانش بنيان
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        بازنگری محتوایی برنامه های ترویج و آموزش کشاورزی و نحوه ارایه خدمات جهت انطباق با تحولات: تغییرات
-                        اقلیمی ، خشکسالی و همه گیری ویروس کووید ۱۹  جهت ارتقاء سطح دانش و بینش روستاییان
-                    </li>
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        شناسایی ویژگی های نوین نظام ترویج و آموزش کشاورزی پايدار در مقابله با شرایط بحرانی: تغییرات اقلیمی،
-                        خشکسالی و همه گیری ویروس کووید ۱۹
-                    </li>
-
-
-                </ul>
-            </div>
-            <div class="row" dir="rtl">
-                <div class="text">
-                    <h3> محورهای کنگره </h3>
-
-                </div>
-
-                <ul class="list" style="list-style: none;">
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        حکمرانی نوین در ترویج و آموزش کشاورزی و توسعه روستایی
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        مسئولیت اجتماعی نظام آموزش عالی کشاورزی
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        الزام های بومی، روندهای آتی و معماری علم ترویج و آموزش کشاورزی، منابع طبیعی و محیط زیست
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        آینده پژوهی نظام های ترویج و آموزش کشاورزی، منابع طبیعی و محیط زیست
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        کارآفرینی کشاورزی، منابع طبیعی و محیط زیست در نظریه و عمل
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        مطالعات تطبیقی آموزش عالی کشاورزی، منابع طبیعی و محیط زیست
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        زیست بوم های توسعه دانش بنیان و فناوری های نوظهور
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        ارتباطات، رسانه و تغییر اجتماعی در کشاورزی
-                    </li>
-
-                </ul>
-
-            </div>
-            <div class="row" dir="rtl">
-                <div class="text">
-                    <h3> تجربیات میدانی موفق </h3>
-
-                </div>
-
-                <ul class="list" style="list-style: none;">
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        ارايه تجربیات و ابتكارات میدانی موفق کارشناسان در عرصه‌هاي کشاورزی ، منابع طبیعی و محیط زیست
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        چالش های میدانی تهدید کننده موفقیت عملکرد کارشناسان پهنه نظام نوین ترویج کشاورزی
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        ارزشیابی و آسيب شناسي عملکرد نظام نوین ترویج کشاورزی و منابع طبیعی (پهنه بندی روستاها)
-                    </li>
-
-                    <li>
-                        <i class="flaticon-tick"></i>
-                        راهکارهای عملي ارتقاء انگیزه خدمت به کشاورزان و روستاییان از نظر کارشناسان ترویج کشاورزی
-                        راه كارهاي پايداري فعاليت بخش غيردولتي ترويج كشاورزي و منابع طبيعي
-
-                    </li>
-
-
-
-
-                </ul>
-            </div>
-        </div>
-    </section>
-    <!-- End Features Area -->
-
-
-    <!-- Start Blog Area -->
-
-    <!-- End Blog Area -->
 @endsection
