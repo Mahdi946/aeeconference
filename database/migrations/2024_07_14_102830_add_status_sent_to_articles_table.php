7264fa6d27e8306f9bd71f73f4c41db38f5c901b
Amin Sepehri <aminrpg@gmail.com>
2024-07-22 16:41:38 +0330

    Update 2024_07_14_102830_add_status_sent_to_articles_table.php

--- database/migrations/2024_07_14_102830_add_status_sent_to_articles_table.php
+++ database/migrations/2024_07_14_102830_add_status_sent_to_articles_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
     {
         Schema::table('articles', function (Blueprint $table) {
             //
-            $table->dropColumn('status');
+            $table->dropColumn('Status');
         });
     }
 };
