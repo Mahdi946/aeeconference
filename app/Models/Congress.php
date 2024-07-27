6a3eeb632b49734ed7f3fb22baf1e808dddca972
taha <tahamzahry@gmail.com>
2024-07-27 09:41:49 +0430

    Update Congress.php

--- app/Models/Congress.php
+++ app/Models/Congress.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Congress extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function secretary()
+    {
+        return $this->belongsTo(User::class , 'SecretaryID');
+    }
 }
