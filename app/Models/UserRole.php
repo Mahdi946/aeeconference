b0d280171a15a6fd990f6973d633cafcab0701a4
taha <tahamzahry@gmail.com>
2024-07-31 15:59:19 +0430

    update model and route

--- app/Models/UserRole.php
+++ app/Models/UserRole.php
@@ -2,11 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserRole extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    public function user()
+    {
+        return $this->belongsTo(User::class,'UserID' );
+    }
+    public function role()
+    {
+        return $this->belongsTo(Role::class , 'RoleID' );
+    }
+
 }
